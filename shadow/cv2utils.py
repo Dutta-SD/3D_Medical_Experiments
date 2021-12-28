a40e3b6bfa997c6f67924540b887d1c14cb04d8b
Dutta-SD <duttasandip11100@gmail.com>
2021-12-28 22:26:29 +0530

    First Exploration, heuristic on hsl

--- shadow/cv2utils.py
+++ shadow/cv2utils.py
@@ -10,11 +10,11 @@ def kmeans_segment(image, K, attempts):
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 
     ret, label, center = cv2.kmeans(
-        twoDimage, 
-        K, 
-        None, 
-        criteria, 
-        attempts, 
+        twoDimage,
+        K,
+        None,
+        criteria,
+        attempts,
         cv2.KMEANS_PP_CENTERS,
         # cv2.KMEANS_RANDOM_CENTERS,
     )
@@ -25,89 +25,78 @@ def kmeans_segment(image, K, attempts):
     return result_image
 
 
-def threshold_binary(img, frac = 0.5, gray=True, inverse=False):
+def threshold_binary(img, frac=0.5, gray=True, inverse=False):
     maxval = np.max(img)
     thresh = maxval * frac
 
     if gray:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     # thresh_method = cv2.THRESH_BINARY_INV if inverse else cv2.THRESH_BINARY
     # thresh_method = cv2.THRESH_OTSU + (cv2.THRESH_BINARY_INV if inverse else cv2.THRESH_BINARY)
     thresh_method = cv2.ADAPTIVE_THRESH_MEAN_C
-    
-    _, im_th = cv2.threshold(
-        img,
-        thresh,
-        maxval,
-        thresh_method
-    )
+
+    _, im_th = cv2.threshold(img, thresh, maxval, thresh_method)
     return im_th
 
 
-def blur(img, kernel_dims = (3, 3)):
+def blur(img, kernel_dims=(3, 3)):
     return cv2.blur(img, kernel_dims)
+
 
 def sharpen_3x3(img):
     filter = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
     return cv2.filter2D(img, -1, filter)
 
-def custom_edge_3x3(img):
-    #edge detection filter
-    kernel = np.array([[0.0, -1.0, 0.0], 
-                    [-1.0, 4.0, -1.0],
-                    [0.0, -1.0, 0.0]])
 
-    kernel = kernel/(np.sum(kernel) if np.sum(kernel)!=0 else 1)
+def custom_edge_3x3(img):
+    # edge detection filter
+    kernel = np.array([[0.0, -1.0, 0.0], [-1.0, 4.0, -1.0], [0.0, -1.0, 0.0]])
+
+    kernel = kernel / (np.sum(kernel) if np.sum(kernel) != 0 else 1)
 
     return cv2.filter2D(img, -1, kernel)
 
-def dilate(img, kernel_size = (2, 2), iter = 1):
+
+def dilate(img, kernel_size=(2, 2), iter=1):
     kernel = np.ones(kernel_size, np.uint8)
     img_dilation = cv2.dilate(img, kernel, iterations=iter)
     return img_dilation
 
+
 def compress_by_2(img):
-    return cv2.resize(
-        img,
-        (0, 0),
-        fx = 0.5,
-        fy=0.5,
-        interpolation=cv2.INTER_AREA
-    )
+    return cv2.resize(img, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+
 
 def expand_by_2(img):
-    return cv2.resize(
-        img,
-        (0,0),
-        fx = 2,
-        fy= 2,
-        interpolation=cv2.INTER_LINEAR
-    )
+    return cv2.resize(img, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+
 
 def hist_eq(img):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16, 16))
     cl1 = clahe.apply(img)
     return cl1
 
+
 def median_blur(img):
     return cv2.medianBlur(img, 3)
 
-def erode(img, itr = 1):
+
+def erode(img, itr=1):
     kernel = np.ones((5, 5), np.uint8)
     return cv2.erode(img, kernel, iterations=itr)
 
+
 def bgr_to_gray(img):
-    return cv2.cvtColor(
-        img,
-        cv2.COLOR_BGR2GRAY
-    )
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 
 def bgr_to_hls(img):
     return cv2.cvtColor(
         img,
         cv2.COLOR_BGR2HLS,
     )
+
 
 def normalize(img):
     M = np.max(img)
@@ -116,11 +105,21 @@ def normalize(img):
     img = ((img - m) / (M - m)) * 255
     return np.array(img, dtype=np.uint8)
 
-def gamma_corr(img, gamma = 0.7):
+
+def gamma_corr(img, gamma=0.7):
     i = img.copy()
-    i = (i / 255)**gamma
-    i = np.array(
-        i * 255,
-        dtype = np.uint8
-    )
+    i = (i / 255) ** gamma
+    i = np.array(i * 255, dtype=np.uint8)
     return i
+
+
+def transform1(img):
+    res = bgr_to_gray(img)
+    res = hist_eq(res)
+    res = compress_by_2(res)
+    res = dilate(res, kernel_size=(3, 3), iter=2)
+    res = kmeans_segment(res, 10, 20)
+    res = expand_by_2(res)
+    res = threshold_binary(res, inverse=True)
+
+    return res
--- shadow/exploration.py
+++ shadow/exploration.py
@@ -1,29 +1,23 @@
 import cv2
 import numpy as np
 import cv2utils as u
+import os
+import config
 
-def transform1(img):
-    res = u.bgr_to_gray(img)
-    res = u.hist_eq(res)
-    res = u.compress_by_2(res)
-    res = u.dilate(res, kernel_size=(3,3), iter=2)
-    res = u.kmeans_segment(res, 10, 20)
-    res = u.expand_by_2(res)
-    res = u.threshold_binary(res, inverse=True)
 
-    return res
-
-def transform2(img):
+def transform2(img, gamma):
     res = img.copy()
     B, G, R = cv2.split(res)
-    R = u.gamma_corr(R, gamma = 0.2)
+    R = u.gamma_corr(R, gamma=gamma)
     res = cv2.merge([B, G, R])
     return res
+
 
 def transform3(img):
     res = u.bgr_to_hls(img.copy())
     B, G, R = cv2.split(res)
     return B
+
 
 def view(img, x):
     cv2.imshow("Original", img)
@@ -32,7 +26,18 @@ def view(img, x):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 # GrayScale
-img = cv2.imread("./ISTD/Img_shadow/5-1.png")
-x = transform3(transform2(img))
-view(img, x)
+if __name__ == "__main__":
+    shadow_img_path = config.BASE_DIR / "ISTD/Img_shadow"
+
+    for file in os.listdir(shadow_img_path):
+        file_name = file.split(".")[0]
+        img = cv2.imread(os.path.join(shadow_img_path, file))
+
+        for gamma in [0.1, 0.2, 0.5, 0.75, 0.8, 1.1, 1.3, 1.4, 1.7, 2.0, 2.3, 3.1]:
+            pth = os.path.join(config.BASE_DIR / "ISTD", "Img_temp", file_name)
+            if not os.path.exists(pth):
+                os.makedirs(pth)
+            x = transform3(transform2(img, gamma))
+            cv2.imwrite(os.path.join(pth, f"{gamma}__{file}"), x)
